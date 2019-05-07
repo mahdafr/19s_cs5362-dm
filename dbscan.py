3f2f4bb0604d63c5d0c8b4fd84d09af2a369756c
Mahdokht Afravi <mmafravi@gmail.com>
2019-05-07 11:58:54 -0600

    dbscan calculates homogeneity, bugs fixed (sorry)

--- dbscan.py
+++ dbscan.py
@@ -3,17 +3,20 @@
 # cs5362 project
 
 import numpy as np
+import random as rand
+import json
 from scipy.sparse import load_npz
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 
-eps = 2
-minPts = 100
+eps = 6
+minPts = 1500
+fdir = 'D:/dm_dataset/'
+subset1 = '5K articles each, 20K features'
+subset2 = '10K articles each, 20K features'
 file = '30K articles each, 20K features'
-subset1 = '30K articles each, 40K features'
-subset2 = '60K articles each, 20K features'
-subset3 = '60K articles each, 40K features'
-fname = 'D:/dm_dataset/' + file + '.npz'
+subset3 = '60K articles each, 20K features'
+fname = fdir + file + '.npz'
 
 print('Running DBSCAN on file: ' + fname)
 print('Using eps-value of: %0.2f' % eps)
@@ -23,7 +26,10 @@ print('Using minPts-value of: %d' % minPts)
 # To load saved matrix file
 sparse_matrix = load_npz(fname)
 sparse_matrix.todense()
-print(sparse_matrix.shape)
+
+# To load saved json file
+# with open(fdir + file + '.json') as json_data:
+#     article_contents = json.load(json_data)
 
 # #############################################################################
 # Compute DBSCAN
@@ -37,14 +43,21 @@ labels = db.labels_
 NUM_ARTICLES = labels.shape[0]
 print('Number of articles: %d' % NUM_ARTICLES)
 labels_true = np.zeros(shape=NUM_ARTICLES)
-labels_true[:NUM_ARTICLES-1] = 0
-labels_true[NUM_ARTICLES:2*NUM_ARTICLES-1] = 1
-labels_true[2*NUM_ARTICLES:3*NUM_ARTICLES-1] = 2
-labels_true[3*NUM_ARTICLES:] = 3
+labels_true[:NUM_ARTICLES-1] = 1
+labels_true[NUM_ARTICLES:2*NUM_ARTICLES-1] = 2
+labels_true[2*NUM_ARTICLES:3*NUM_ARTICLES-1] = 3
+labels_true[3*NUM_ARTICLES:] = 4
 
 # Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 n_noise_ = list(labels).count(-1)
+
+# print clusters info to file
+clusters = set(labels)
+file = open('_test.txt','a')
+for i in range(db.components_.shape[0]):
+    file.write(str(db.components_[i]) + '\n')
+file.close()
 
 # #############################################################################
 # Results of DBSCAN and metrics
