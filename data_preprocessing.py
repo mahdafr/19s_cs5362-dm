42cfc670ef3cb490acf4dd60895b606c63f09304
Jonathan Avila <jeavila6@miners.utep.edu>
2019-05-08 10:43:39 -0600

    undo bad merge

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -4,54 +4,49 @@ Created on ~
 
 @author: Gerardo Cervantes
 """
-
-import json
+import argparse
+import csv
 import nltk
 from nltk.stem import PorterStemmer
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords 
 import string
 import numpy as np
+from collections import Counter
+import os
+import csv_const
+
 import scipy
 
-# Tokenizes the articles, and cleans the input
-# Returns list of lists of size (nArticles, nWordsInArticle)
+stop_words = set(stopwords.words('english'))
+punctuation = string.punctuation + '“”’—–'
+
+
+# returns list of lists of clean tokens
 def clean_articles(article_contents):
-    ps = PorterStemmer()
-    # lemmatizer = WordNetLemmatizer()
-    stop_words = set(stopwords.words('english'))
 
     cleaned_article_contents = []
+
     for content in article_contents:
-        content = content.rstrip('\n')  # removes \n
-        # Efficient way to remove punctuation
-        content = content.translate(str.maketrans('', '', string.punctuation))
-        content = content.lower()  # lowercase
-        tokens = nltk.word_tokenize(content)
-        cleaned_content = [ps.stem(w) for w in tokens if not w in stop_words]
-        # lemmatized_word = lemmatizer.lemmatize(word)
+        content = content.lower()  # to lowercase
+        content = content.translate(str.maketrans('', '', punctuation))  # remove punctuation
+        tokens = nltk.word_tokenize(content)  # tokenize
+        cleaned_content = [PorterStemmer().stem(token) for token in tokens if token not in stop_words]  # stem
         cleaned_article_contents.append(cleaned_content)
+
     return cleaned_article_contents
 
 
-# Creates a vocabulary (dictionary-key is word, value is id) of size vocab_size.
-# TODO could be improved by looking for most common words, for purposes of clustering, it will have little affect
+# create vocabulary dictionary (key is word, value is id) of size vocab_size
 def create_vocab_dict(article_contents, vocab_size):
-    vocab = {}
-    word_id = 0
-    for article in article_contents:
-        for token in article:
-
-            if token not in vocab:
-                # Add to vocab and increment id #
-                vocab[token] = word_id
-                word_id += 1
-                if vocab_size <= word_id:
-                    return vocab
+    words = [word for content in article_contents for word in content]
+    vocab = Counter(words).most_common(vocab_size)           # [('cat', 13), ('inform', 10), ('cnn', 9), ...]
+    vocab = [common_word[0] for common_word in vocab]        # ['cat', 'inform', 'cnn', ...]
+    vocab = {key: value for value, key in enumerate(vocab)}  # {'cat': 0, 'inform': 1, 'cnn': 2, ...}
     return vocab
 
 
 # Returns 2D matrix of size (nSamples, vocabSize) value is word frequency
-def create_one_hot_encoded_matrix(article_contents, vocab):
+def create_encoded_matrix(article_contents, vocab):
     vocab_size = len(vocab)
     articles_matrix = []
     for article in article_contents:
@@ -59,32 +54,46 @@ def create_one_hot_encoded_matrix(article_contents, vocab):
         for token in article:
             if token in vocab:
                 article_vector[vocab[token]] += 1
-
         articles_matrix.append(article_vector)
     articles_matrix = np.array(articles_matrix)
     return articles_matrix
 
 
 if __name__ == "__main__":
-    file_name = 'fake'
-    data_path = 'D:/dm_dataset/' + file_name + '.json'
 
-    with open(data_path) as json_data:
-        conts = json.load(json_data)
-    article_contents = conts[:15000] # 30000, 50000, 70000, 80000
-    print('Finished reading JSON file')
-    vocab_size = 20000 # 40k is a good size - too much and unknown words will show up
-    tokenized_articles = clean_articles(conts)
-    print('Finish cleaning and tokenizing articles')
-    vocab = create_vocab_dict(tokenized_articles, vocab_size)
-    print('Created vocab of size ' + str(vocab_size))
-    articles_matrix = create_one_hot_encoded_matrix(tokenized_articles, vocab)
-    np.save(file_name + '_dict.npy', vocab)
-    print('Finished converting articles to a matrix')
-    sparse_matrix = scipy.sparse.csc_matrix(articles_matrix)
-    print('Converted to sparse')
-    scipy.sparse.save_npz(file_name, sparse_matrix)
-    print('Saved matrix to file')
-    # To load saved matrix file:
+    parser = argparse.ArgumentParser(description='get matrix')
+    parser.add_argument('-filename', type=str, required=True, help='.csv file')
+    parser.add_argument('-article_limit', type=int, required=True, help='number of articles to read')
+    parser.add_argument('-vocabulary_size', type=int, required=True, default=40000, help='number of words in vocabulary')
+    args = parser.parse_args()
+
+    filename_base = os.path.splitext(args.filename)[0]
+
+    # default vocab size is 40k - too much and unknown words will show up
+
+    csv.field_size_limit(2147483647)  # avoid errors on huge fields
+
+    contents = []
+    with open(args.filename, encoding=csv_const.ENCODING, newline=csv_const.NEWLINE) as csv_file:  # csv for reading
+        reader = csv.reader(csv_file, delimiter=csv_const.DELIMITER)
+        count = 0  # article limit could be more Pythonic...
+        for row in csv.reader(csv_file, delimiter=csv_const.DELIMITER):
+            fields = dict(zip(csv_const.FIELDS, row))
+            contents.append(fields['content'])
+            count += 1
+            if count >= args.article_limit:
+                break
+
+    tokenized_articles = clean_articles(contents)  # clean and tokenize article contents
+    vocabulary = create_vocab_dict(tokenized_articles, args.vocabulary_size)  # create vocabulary
+    np.save('vocab_dict.npy', vocabulary)
+    articles_matrix = create_encoded_matrix(tokenized_articles, vocabulary)  # convert to matrix
+    sparse_matrix = scipy.sparse.csc_matrix(articles_matrix)  # convert to sparse matrix
+
+    # save sparse matrix
+    filename_base = os.path.splitext(args.filename)[0]
+    scipy.sparse.save_npz(filename_base, sparse_matrix)
+
+    # to load saved matrix file:
     # sparse_matrix = scipy.sparse.load_npz(file_name)
     # sparse_matrix.todense()
